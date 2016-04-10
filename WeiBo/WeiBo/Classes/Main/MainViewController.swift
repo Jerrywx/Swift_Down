97bd436af485da86f574b08b5c3d2fa93afdfaad
王潇 <23601145@qq.com>
2016-04-10 21:21:56 +0800

    1. 动态加载控制器

--- WeiBo/WeiBo/Classes/Main/MainViewController.swift
+++ WeiBo/WeiBo/Classes/Main/MainViewController.swift
@@ -12,15 +12,29 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-		// 1. 设置 tabBar 颜色
-		tabBar.tintColor = UIColor.orangeColor()
-		// 2. 添加子控制器
-		addChildViewController(HomeViewController(), title: "首页", imageName: "tabbar_home");
-		addChildViewController(DiscoverViewController(), title: "发现", imageName: "tabbar_discover")
-		addChildViewController(MessageViewController(), title: "消息", imageName: "tabbar_message_center")
-		addChildViewController(ProfileViewController(), title: "我", imageName: "tabbar_profile")
+		addChildViewControllers()
     }
 
+	private func addChildViewControllers() {
+		// 1. 读取配置文件
+		let jsonPath = NSBundle.mainBundle().pathForResource("MainVCSettings.json", ofType: nil)
+		let jsonData = NSData(contentsOfFile: jsonPath!)
+		// 2. 解析配置文件
+		do {
+			// 使用 try/catch 捕获异常
+			let dictArray = try NSJSONSerialization.JSONObjectWithData(jsonData!, options: NSJSONReadingOptions.MutableContainers)
+			// 遍历数组 必行明确数组的类型
+			for dict in dictArray as! [[String: String]] {
+				addChildViewController(dict["vcName"]!, title: dict["title"]!, imageName: dict["imageName"]!)
+			}
+		} catch {
+			addChildViewController("HomeViewController",	 title: "首页", imageName: "tabbar_home")
+			addChildViewController("MessageViewController",	 title: "消息", imageName: "tabbar_message_center")
+			addChildViewController("DiscoverViewController", title: "发现", imageName: "tabbar_discover")
+			addChildViewController("ProfileViewController",  title: "我",   imageName: "tabbar_profile")
+		}
+	}
+	
 	/**
 	初始化子控制器
 	
@@ -28,14 +42,26 @@ class MainViewController: UITabBarController {
 	- parameter title:           标题
 	- parameter imageName:       图片名称
 	*/
-	private func addChildViewController(childController: UIViewController, title: String, imageName: String) {
-		// 1. 初始化子控制器
-		childController.tabBarItem.image = UIImage(named: imageName)
-		childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
-		childController.title = title
-		// 2.
-		let nav = UINavigationController(rootViewController: childController);
-		addChildViewController(nav)
+	private func addChildViewController(childControllerName: String, title: String, imageName: String) {
+		
+		// 0. 动态获取命名空间
+		let nameSpace = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as! String
+		print("命名空间: \(nameSpace)")
+		
+		// 2. 根据类名创建子控制器类
+		let childClass:AnyClass = NSClassFromString(nameSpace + "." + childControllerName)!
+		let childType = childClass as! UIViewController.Type
+		let childVC = childType.init()
+		
+		// 3. 设置 tabBar 属性
+		childVC.title = title
+		childVC.tabBarItem.image = UIImage(named: imageName)
+		childVC.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+		tabBar.tintColor = UIColor.orangeColor()
+		
+		// 4. 创建导航控制器
+		let childNav = UINavigationController(rootViewController: childVC)
+		addChildViewController(childNav)
 	}
 }
 
