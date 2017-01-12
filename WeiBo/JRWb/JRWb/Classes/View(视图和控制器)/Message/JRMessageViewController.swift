54c045d9c51ee03061e5b7ebfab199e97f43c93b
Jerrywx <23601145@qq.com>
2017-01-12 08:51:10 +0800

    目录结构

---
+++ WeiBo/JRWb/JRWb/Classes/View(视图和控制器)/Message/JRMessageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  JRMessageViewController.swift
+//  JRWb
+//
+//  Created by 王潇 on 2017/1/12.
+//  Copyright © 2017年 王潇. All rights reserved.
+//
+
+import UIKit
+
+class JRMessageViewController: JRBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
