fc75246ef05db5040585adfb8917f0c851f1fbf3
Jerrywx <23601145@qq.com>
2017-01-12 08:52:53 +0800

    添加基础文件

---
+++ WeiBo/JRWb/JRWb/Classes/ViewModel(视图模型)/JRBaseViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  JRBaseViewModel.swift
+//  JRWb
+//
+//  Created by 王潇 on 2017/1/12.
+//  Copyright © 2017年 王潇. All rights reserved.
+//
+
+import UIKit
+
+class JRBaseViewModel: NSObject {
+
+}
