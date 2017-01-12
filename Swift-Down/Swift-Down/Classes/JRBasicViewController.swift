d6ac53ab4536e88cb07c4f1cd8c684921c203c68
Jerrywx <23601145@qq.com>
2017-01-12 11:32:55 +0800

    修改 gitignore

---
+++ Swift-Down/Swift-Down/Classes/JRBasicViewController.swift
@@ -0,0 +1,20 @@
+//
+//  JRBasicViewController.swift
+//  Swift-Down
+//
+//  Created by 王潇 on 2017/1/10.
+//  Copyright © 2017年 王潇. All rights reserved.
+//
+
+import UIKit
+
+class JRBasicViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+	
+	
+}
