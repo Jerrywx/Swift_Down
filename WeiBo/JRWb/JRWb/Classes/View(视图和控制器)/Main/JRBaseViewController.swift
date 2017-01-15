3dceca611dd3da9902f82aecb23132d77df6603f
Jerrywx <23601145@qq.com>
2017-01-15 08:36:16 +0800

    重写navigationBar & title Set方法

--- WeiBo/JRWb/JRWb/Classes/View(视图和控制器)/Main/JRBaseViewController.swift
+++ WeiBo/JRWb/JRWb/Classes/View(视图和控制器)/Main/JRBaseViewController.swift
@@ -10,10 +10,24 @@ import UIKit
 
 class JRBaseViewController: UIViewController {
 
+	lazy var navBar = UINavigationBar(frame: CGRect(x: 0, 
+	                                                y: 0, 
+	                                                width: UIScreen.main.bounds.width, 
+	                                                height: 64))
+	
+	lazy var navItem = UINavigationItem()
+	
+	override var title: String? {
+		didSet {
+			navItem.title = title
+		}
+	}
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-
+		/// 初始化 UI
 		setupUI()
+		
     }
 }
 
@@ -22,5 +36,7 @@ extension JRBaseViewController {
 	
 	func setupUI() {
 		view.backgroundColor = UIColor.cz_random()
+		view.addSubview(navBar)
+		navBar.items = [navItem]
 	}	
 }
--- WeiBo/JRWb/JRWb/Classes/View(视图和控制器)/Main/JRNavigationController.swift
+++ WeiBo/JRWb/JRWb/Classes/View(视图和控制器)/Main/JRNavigationController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class JRNavigationController: UINavigationController {
 
+	/// 重写 viewDidLoad
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		navigationBar.isHidden = true
+	}
+	
+	/// 重写push 方法
+	///
+	/// - Parameters:
+	///   - viewController: push控制器
+	///   - animated: 是否动画
 	override func pushViewController(_ viewController: UIViewController, animated: Bool) {
 
 		/// 判断是不是 root 控制器
