8153095653d7a814a35e24a3a2f30c53769f6484
Jerrywx <23601145@qq.com>
2017-01-15 23:54:49 +0800

    Swift 笔记

--- Headers/Swift Guide.playground/Pages/Class and Struct(类和结构体).xcplaygroundpage/Contents.swift
+++ Headers/Swift Guide.playground/Pages/Class and Struct(类和结构体).xcplaygroundpage/Contents.swift
@@ -1,7 +1,26 @@
-//: [Previous](@previous)
+/*:
+[Swift Guide](Swift%20Guide(%E5%BC%95%E5%AF%BC)) | [Previous](@previous) | [Next](@next)
+****
+*/
 
 import Foundation
 
-var str = "Hello, playground"
 
-//: [Next](@next)
+//: 1. 类个结构体对比
+
+//: 2. 结构体和枚举是值类型
+
+//: 3. 类是引用类型
+
+//: 4. 类和结构体的选择
+
+//: 5. 字符串、数组和字典类型的赋值行为
+
+
+
+
+
+/*:
+****
+[Swift Guide](Swift%20Guide(%E5%BC%95%E5%AF%BC)) | [Previous](@previous) | [Next](@next)
+*/
