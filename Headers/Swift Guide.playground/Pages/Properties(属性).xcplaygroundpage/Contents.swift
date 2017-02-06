e2cd692040bcac0014aa1ca8be18bf9fe55011c4
Jerrywx <23601145@qq.com>
2017-02-06 23:30:50 +0800

    属性观察器

--- Headers/Swift Guide.playground/Pages/Properties(属性).xcplaygroundpage/Contents.swift
+++ Headers/Swift Guide.playground/Pages/Properties(属性).xcplaygroundpage/Contents.swift
@@ -72,7 +72,7 @@ let rangeOfLet = FixedLengthRange(firstValue: 0, length: 4)
 class DataImporter {
 	var fileName = "data.txt"
 	init() {
-		print("加载数据")
+//		print("加载数据")
 	}
 }
 
@@ -98,6 +98,135 @@ manager.importer
 //: “Swift 中的属性没有对应的实例变量，属性的后端存储也无法直接访问。这就避免了不同场景下访问方式的困扰，同时也将属性的定义简化成一个语句。属性的全部信息——包括命名、类型和内存管理特征——都在唯一一个地方（类型定义中）定义。”
 
 
+
+/*:
+	二、计算属性
+****
+	计算属性不直接存储值, 而是提供一个getter和一个可选的setter, 来简介获取和设置其他属性或变量的值。
+
+*/
+/// 声明点 结构体
+struct Point {
+	var x = 0.0, y = 0.0
+}
+
+/// 声明 尺寸结构体
+struct Size {
+	var width = 0.0, height = 0.0;
+}
+
+/// 声明 Rect 
+struct Rect {
+	var origin = Point()
+	var size = Size()
+	var center: Point {
+		get {
+			let centerX = origin.x + (size.width / 2)
+			let centerY = origin.y + (size.height / 2)
+			return Point(x: centerX, y: centerY)
+		}
+		
+		set(newCenter) {
+			origin.x = newCenter.x - (size.width / 2)
+			origin.y = newCenter.y - (size.height / 2)
+		}
+	}
+}
+
+/// 使用
+
+var square = Rect(origin: Point(x: 0.0, y: 0.0), 
+                  size: Size(width: 10.0, height: 10.0))
+let initialSquareCenter = square.center
+print(initialSquareCenter)
+
+
+//: 1、简化setter声明
+
+//: 如果计算属性的 setter 没有定义表示新值得参数, 则可以使用默认名称 newValue.
+
+
+//: 2、只读计算属性
+//:
+//: 只有getter 没有setter 的计算属性就是只读计算属性, 只读计算属性总是返回一个值, 可以通过点运算个访问, 但不能设置新的值。
+//:
+//: 注意: 必须使用 var 关键字定义计算属性, 包括只读计算属性, 因为它们的值不是固定的。let 关键之只能用来声明常量属性, 表示初始化之后再也无法修改的值。
+/// 只读计算属性
+struct Cuboid {
+	var width = 0.0, height = 0.0, depth = 0.0
+	var valume: Double {
+		return width * height * depth
+	}
+}
+
+/// 使用
+let fourBtFiveByTwo = Cuboid(width: 4, height: 5, depth: 2)
+print(fourBtFiveByTwo.valume)
+
+
+
+/*:
+	三、属性观察器
+****
+	1. 声明是属性观察器？
+	属性观察器监控和响应属性的变化, 每次属性被设置值得时候都会调用属性观察器, 即使新值和当前值相同的时候也不例外。
+
+	2. 属性观察器:
+	willSet 在新的值被设置之前调用
+	willSet 观察器会将新的属性值作为常量参数传入, 在willSet的实现代码中可以为这个参数指定一个名词, 如果不指定则参数任然可用, 这只使用默认名词 newValue 表示。
+
+	didSet 在新的值被设置之后立即调用
+	didSet 观察器会将旧的属性作为参数传入, 可以为该参数命名或者使用默认参数名 oldValue。
+
+	3. 注意
+	父类的属性在子类的构造器中被赋值时, 它在父类中的 willSet 和 didSet观察器会被调用, 随后才会调用子类的观察器。
+*/
+
+/// 计数类
+class StepCounter {
+	var totalSteps: Int = 0 {
+		willSet(newTotalSteps) {
+			print("About to set totalSteps to \(newTotalSteps)")
+		}
+		didSet {
+			if totalSteps > oldValue  {
+				print("Added \(totalSteps - oldValue) steps")
+			}
+		}
+	}
+}
+
+/// 使用
+let stepCounter = StepCounter()
+//stepCounter.totalSteps = 200
+//stepCounter.totalSteps = 360
+//stepCounter.totalSteps = 896
+
+/// 计数子类
+class SubCounter: StepCounter {
+	override var totalSteps: Int {
+		willSet{
+			print("Sub WillSet: \(newValue)")
+		}
+		
+		didSet {
+			print("Sub didSet: \(oldValue)")
+		}
+	}
+}
+
+/// 使用
+let counter = SubCounter()
+counter.totalSteps = 200
+
+
+/*
+	四、全局变量和局部变量
+****
+	1. 什么是全局变量和局部变量？
+	
+
+*/
 
 
 /*:
