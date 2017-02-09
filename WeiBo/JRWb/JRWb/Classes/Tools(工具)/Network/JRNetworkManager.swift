1c9118879838dad2ac2c2dae200d0d1775403794
Jerrywx <23601145@qq.com>
2017-02-09 18:20:24 +0800

    添加网络单粒

---
+++ WeiBo/JRWb/JRWb/Classes/Tools(工具)/Network/JRNetworkManager.swift
@@ -0,0 +1,18 @@
+//
+//  JRNetworkManager.swift
+//  JRWb
+//
+//  Created by 王潇 on 2017/2/9.
+//  Copyright © 2017年 王潇. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+/// 网络管理工具
+class JRNetworkManager: AFHTTPSessionManager {
+
+	/// 网络单粒
+	static let shared = JRNetworkManager()
+	
+}
