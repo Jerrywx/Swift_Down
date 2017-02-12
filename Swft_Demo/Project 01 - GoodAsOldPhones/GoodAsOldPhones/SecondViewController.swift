eecbda67bd8fa181ffd6c5a5ffc550f2ecdf5b9f
Jerrywx <23601145@qq.com>
2017-02-12 17:01:26 +0800

    GoodAsOldPhones 80%

--- Swft_Demo/Project 01 - GoodAsOldPhones/GoodAsOldPhones/SecondViewController.swift
+++ Swft_Demo/Project 01 - GoodAsOldPhones/GoodAsOldPhones/SecondViewController.swift
@@ -10,10 +10,50 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+	///
+	var scrollView:UIScrollView?
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1)
+        setupUI()
     }
 	
+}
+
+// MARK: - 初始化界面
+extension SecondViewController {
+	
+	fileprivate func setupUI() {
+		view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+		scrollView = UIScrollView(frame: CGRect(x: 0, 
+		                                        y: 0, 
+		                                        width: self.view.bounds.width,
+		                                        height: self.view.bounds.height - 46))
+		view.addSubview(scrollView!)
+		scrollView?.backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+		
+		addSubViews()
+	}
+	
+	private func addSubViews() {
+		
+		let width = view.bounds.width
+		/// 图片
+		let image	 = #imageLiteral(resourceName: "header-contact")
+		let imgFrame = CGRect(x: 0, y: 0, 
+		                      width: width,
+		                      height: width / (image.size.width / image.size.height))
+		let imageView	= UIImageView(frame: imgFrame)
+		imageView.image = image
+		scrollView?.addSubview(imageView)
+		
+		/// 
+		let titleY = imgFrame.maxY + 60
+		let frame = CGRect(x: 0, y: titleY, width: width, height: 30)
+		let title = UILabel(frame: frame)
+		title.textAlignment = .center
+		title.text = "About me"
+		title.font = UIFont(name: "DamascusBold", size: 24)
+		view.addSubview(title)
+	}
 }
--- Swft_Demo/Project 01 - GoodAsOldPhones/GoodAsOldPhones/ViewController.swift
+++ Swft_Demo/Project 01 - GoodAsOldPhones/GoodAsOldPhones/ViewController.swift
@@ -12,6 +12,18 @@ class ViewController: UIViewController {
 
 	/// 属性
 	var tableView:UITableView?
+	var products = [Product(name: "1907 Wall Set", 
+	                        cellImageName: "image-cell1", 
+	                        fullscreenImageName: "phone-fullscreen1"),
+	                Product(name: "1921 Dial Phone", 
+	                        cellImageName: "image-cell2", 
+	                        fullscreenImageName: "phone-fullscreen2"),
+	                Product(name: "1937 Desk Set", 
+	                        cellImageName: "image-cell3", 
+	                        fullscreenImageName: "phone-fullscreen3"),
+	                Product(name: "1984 Moto Portable", 
+	                        cellImageName: "image-cell4", 
+	                        fullscreenImageName: "phone-fullscreen4")]
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -22,16 +34,16 @@ class ViewController: UIViewController {
 
 // MARK: - 初始化界面
 extension ViewController {
-
+	
 	/// 初始化界面
 	fileprivate func setupUI() {
 		/// 设置背景色
 		view.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
 		/// 初始化TableView
 		tableView = UITableView(frame: view.bounds, style: .grouped)
-		tableView?.dataSource = self
-		tableView?.delegate = self
-		tableView?.rowHeight = 94
+		tableView?.dataSource	= self
+		tableView?.delegate		= self
+		tableView?.rowHeight	= 94
 		tableView?.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
 		view.addSubview(tableView!)
 		
@@ -42,13 +54,24 @@ extension ViewController {
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 4
+		return products.count
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-		cell?.textLabel?.text = "asdas"
+		let p	 = products[indexPath.row]
+		cell?.accessoryType		= .disclosureIndicator
+		cell?.textLabel?.text	= p.name
+		cell?.imageView?.image	= UIImage(named: p.cellImageName!)
 		return cell!
+	}
+	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		tableView.deselectRow(at: indexPath, animated: true)
+		let product = products[indexPath.row]
+		let vc = DetialViewController()
+		vc.product = product
+		self.navigationController?.pushViewController(vc, animated: true)
 	}
 	
 }
